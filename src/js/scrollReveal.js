ea5aedc9ee9560e3290f94bf0c6b4c6fe173e72c
Josué Alisson <bateriadotransformice@gmail.com>
2022-07-26 17:02:14 -0300

    .

---
+++ src/js/scrollReveal.js
@@ -0,0 +1,25 @@
+Window.sr = ScrollReveal({ reset: true })
+
+ScrollReveal().reveal('.container-title',{
+    delay: 200
+});
+
+ScrollReveal().reveal('.container-fotos-list1'),{
+    delay: 2000
+}
+ScrollReveal().reveal('.container-fotos-list2'),{
+    delay: 2000
+}
+ScrollReveal().reveal('.redes-sociais'),{
+    delay: 500
+}
+ScrollReveal().reveal('.informations'),{
+    delay: 1000
+}
+ScrollReveal().reveal('.logo-footer'),{
+    delay: 1000
+}
+ScrollReveal().reveal('.copyright'),{
+    delay: 1000
+}
+
