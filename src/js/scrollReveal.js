bb0c02dc02ba2faab9540770b6c849d9b3810535
Josué Alisson <bateriadotransformice@gmail.com>
2023-01-26 23:13:04 -0300

    Alterando o footer, section e header

--- src/js/scrollReveal.js
+++ src/js/scrollReveal.js
@@ -1,25 +1,80 @@
-Window.sr = ScrollReveal({ reset: true })
+Window.sr = ScrollReveal({
+     reset: false 
+    })
 
-ScrollReveal().reveal('.container-title',{
-    delay: 200
+/* NAV LIST */
+ScrollReveal().reveal('.nav-list_scrollReveal1',{
+    delay:100
+});
+ScrollReveal().reveal('.nav-list_scrollReveal2',{
+    delay:300
+});
+ScrollReveal().reveal('.nav-list_scrollReveal3',{
+    delay:500
+});
+ScrollReveal().reveal('.nav-list_scrollReveal4',{
+    delay:700
 });
 
-ScrollReveal().reveal('.container-fotos-list1'),{
-    delay: 2000
-}
-ScrollReveal().reveal('.container-fotos-list2'),{
-    delay: 2000
-}
-ScrollReveal().reveal('.redes-sociais'),{
+
+/* CARDS */
+ScrollReveal().reveal('.cards',{
+    delay:1000
+});
+
+ScrollReveal().reveal('.card-scrollReveal-1',{
+    delay:1000
+});
+ScrollReveal().reveal('.card-scrollReveal-2',{
+    delay:1200
+});
+ScrollReveal().reveal('.card-scrollReveal-3',{
+    delay:1400
+});
+ScrollReveal().reveal('.card-scrollReveal-4',{
+    delay:1600
+});
+ScrollReveal().reveal('.card-scrollReveal-5',{
+    delay:1000
+});
+ScrollReveal().reveal('.card-scrollReveal-6',{
+    delay:1200
+});
+ScrollReveal().reveal('.card-scrollReveal-7',{
+    delay:1400
+});
+ScrollReveal().reveal('.card-scrollReveal-8',{
+    delay:1600
+});
+/* SECTION */
+ScrollReveal().reveal('.call_to_action_area',{
+    delay: 1000
+});
+ScrollReveal().reveal('.call_to_action_wrapper',{
+    delay: 1200
+});
+ScrollReveal().reveal('.call_to_action_content',{
+    delay: 1200
+});
+
+
+ScrollReveal().reveal('.container-title',{
+    delay: 800
+});
+
+ScrollReveal().reveal('.redes-sociais',{
     delay: 500
-}
-ScrollReveal().reveal('.informations'),{
+});
+
+ScrollReveal().reveal('.informations',{
     delay: 1000
-}
-ScrollReveal().reveal('.logo-footer'),{
-    delay: 1000
-}
-ScrollReveal().reveal('.copyright'),{
-    delay: 1000
-}
+});
+
+ScrollReveal().reveal('.logo-footer',{
+    delay: 1500
+});
+
+ScrollReveal().reveal('.copyright',{
+    delay: 1700
+});
 
